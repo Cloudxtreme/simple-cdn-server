7a4727a2da0b6aa6f3f9dff0f73b3f1614ccb58a
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-07-19 21:13:24 +0200

    Create Accesses CRUD

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -1,4 +1,38 @@
 class AccessesController < ApplicationController
+
   def index
+    @accesses = Access.all
+  end
+
+  def show
+    @access = Access.find(params[:id])
+  end
+
+  def new
+    @access = Access.new
+  end
+
+  def create
+    @access = Access.create(params[:access].permit(:identifier))
+
+    if @access.save
+      redirect_to @access
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @access = Access.find(params[:id])
+  end
+
+  def update
+    @access = Access.find(params[:id])
+
+    if @access.update(params[:access].permit(:identifier))
+      redirect_to @access
+    else
+      render 'edit'
+    end
   end
 end
--- app/models/access.rb
+++ app/models/access.rb
@@ -1,2 +1,5 @@
 class Access < ActiveRecord::Base
+
+  # Validators
+  validates :identifier, uniqueness: true
 end
