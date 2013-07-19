80705fa183054b9cfafb24b97f54b1d0d49cc9e4
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-07-19 21:34:10 +0200

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SimpleCDNServer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :accesses
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
