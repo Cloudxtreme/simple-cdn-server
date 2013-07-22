d8f8e3e342d5382a4c9999c06aca87ac04e54d3e
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-07-22 23:45:56 +0200

    Update access CRUD

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -1,40 +1,74 @@
 class AccessesController < ApplicationController
 
+  # GET /
   def index
     @accesses = Access.all
   end
 
+  # GET /accesses/:id
   def show
     @access = Access.find(params[:id])
+
+    SimpleCDN::Configurator::CDN.calculate_size! @access # TODO move this line into ftpd
   end
 
+  # GET /accesses/new
   def new
     @access = Access.new
   end
 
+  # POST /accesses
   def create
-    @access = Access.create(params[:access].permit(:identifier))
+    @access = Access.create(get_params)
 
     if @access.save
       SimpleCDN::Configurator::CDN.create(@access)
+      SimpleCDN::Configurator::Nginx.create(@access)
+
       redirect_to @access
     else
       render 'new'
     end
   end
 
+  # GET /accesses/:id/edit
   def edit
     @access = Access.find(params[:id])
   end
 
+  # PUT   /accesses/:id
+  # PATCH /accesses/:id
   def update
     @access = Access.find(params[:id])
 
-    if @access.update(params[:access].permit(:identifier))
+    if @access.update(get_params)
       SimpleCDN::Configurator::CDN.update(@access)
+      SimpleCDN::Configurator::Nginx.update(@access)
+
       redirect_to @access
     else
       render 'edit'
     end
+  end
+
+  # DELETE /accesses/:id
+  def destroy
+    @access = Access.find(params[:id])
+
+    unless SimpleCDN::Configurator::CDN.is_empty?(@access)
+      flash[:error] = :access_cant_be_destroy
+    else
+      @access.destroy
+      SimpleCDN::Configurator::CDN.destroy!(@access)
+      flash[:notice] = :access_destroy
+    end
+
+    redirect_to accesses_path
+  end
+
+private
+
+  def get_params
+    params.require(:access).permit(:identifier, :domain, :quotas)
   end
 end
--- app/models/access.rb
+++ app/models/access.rb
@@ -1,5 +1,8 @@
 class Access < ActiveRecord::Base
 
+  # Filters
+  before_save :default_values
+
   # Validators
   validates :identifier,    uniqueness: true,
                             presence: true
@@ -7,4 +10,16 @@ class Access < ActiveRecord::Base
                             presence: true
   validates :quotas,        presence: true
 
+
+  def default_values
+    self.size   ||= 0
+    self.quotas ||= 0
+  end
+
+  def ratio_use
+    return 0 if self.size.blank?
+    return 0 if self.quotas.blank?
+
+    self.size / self.quotas
+  end
 end
