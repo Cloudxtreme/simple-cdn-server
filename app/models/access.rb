8362fdef6b955381a390e136686f2f19d3f86c98
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-07-22 23:45:25 +0200

    Update validators on Access model

--- app/models/access.rb
+++ app/models/access.rb
@@ -1,5 +1,10 @@
 class Access < ActiveRecord::Base
 
   # Validators
-  validates :identifier, uniqueness: true
+  validates :identifier,    uniqueness: true,
+                            presence: true
+  validates :domain,        uniqueness: true,
+                            presence: true
+  validates :quotas,        presence: true
+
 end
